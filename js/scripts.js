81c9cea220d2b390b7a65e0b5ae71736cdb9e3dc
Sykko <80289020+Sykko95@users.noreply.github.com>
2021-10-22 18:16:23 -0300

    Removed unnecessary function

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,3 @@ function isNumberKey(evt){
 	    return false;
 	return true;
 }
-
-function isNumberKeyDec(evt){
-	var charCode = (evt.which) ? evt.which : evt.keyCode
-	if (charCode > 31 && (charCode != 46 &&(charCode < 48 || charCode > 57)))
-	    return false;
-	return true;
-}
